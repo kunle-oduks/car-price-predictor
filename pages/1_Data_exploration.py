1ab7714193c838da65fa27aa7520cdda85fe8ef9
kunle-oduks <119300177+kunle-oduks@users.noreply.github.com>
2024-04-10 23:34:55 +0100

    Update 1_Data_exploration.py

--- pages/1_Data_exploration.py
+++ pages/1_Data_exploration.py
@@ -24,21 +24,21 @@ def plotter(dataframe, col1, col2, col3, dep):
     plt.figure(figsize=(20,8))
 
     plt.subplot(1,3,1)
-    st.subheader(f"{col1} vs {col2}, vs {dep}")
+    st.subheader(f"{col1} vs {col2} vs {dep}")
     col1plot = px.scatter_3d(data_frame= dataframe, x = col1, z  = col2, y = dep)
     st.plotly_chart(col1plot)
     
     st.markdown('<br>',  unsafe_allow_html=True)
 
     plt.subplot(1,3,2)
-    st.subheader(f"{col2} vs {col3}, {dep}")
+    st.subheader(f"{col2} vs {col3} vs {dep}")
     col2plot = px.scatter_3d(data_frame= dataframe, x = col2, y=dep, z=col3)
     st.plotly_chart(col2plot)
   
     st.markdown('<br>',  unsafe_allow_html=True)
 
     plt.subplot(1,3,3)
-    st.subheader(f"{col3} vs {col1}, {dep}")
+    st.subheader(f"{col3} vs {col1} vs {dep}")
     col3plot = px.scatter_3d(data_frame= dataframe, x = col3, y = dep, z=col1)
     st.plotly_chart(col3plot)
 
